c420061d6399fa8d46d1c5dfc8fe5d2e3a9b59b3
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2024-01-23 17:23:48 +0400

    add the example solution

---
+++ CS/ExtendedGridLayout/Data/LocalStorageHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace ExtendedGridLayout.Data {
+    public class LocalStorageHelper {
+        private readonly IJSRuntime _JSRuntime;
+        public LocalStorageHelper(IJSRuntime JSRuntime) {
+            _JSRuntime = JSRuntime;
+        }
+        public  async Task<string> GetItemAsync(string key) {
+            try {
+                return await _JSRuntime.InvokeAsync<string>("localStorage.getItem", key);
+            }
+            catch {
+                // Mute exceptions
+                return "";
+            }
+        }
+        public async Task SetItemAsync(string key, string value) {
+            try {
+                await _JSRuntime.InvokeVoidAsync("localStorage.setItem", key, value);
+            }
+            catch {
+                // Mute exceptions
+            }
+        }
+    }
+}
---
+++ CS/ExtendedGridLayout/Data/UserLayout.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.Blazor;
+using DevExpress.Blazor.Internal;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ExtendedGridLayout.Data {
+    public class UserLayout {
+        public string LayoutKey { get; set; }
+        public GridExtendedLayout Layout { get; set; }
+    }
+
+    public class GridExtendedLayout {
+        public GridPersistentLayout Layout { get; }
+        public bool FilterRowVisible { get; }
+        public bool GroupPanelVisible { get; }
+        public bool SearchBoxVisible { get; }
+
+        public GridExtendedLayout(GridPersistentLayout layout, bool filterRowVisible, bool groupPanelVisible, bool searchBoxVisible) {
+            Layout = layout;
+            FilterRowVisible = filterRowVisible;
+            GroupPanelVisible = groupPanelVisible;
+            SearchBoxVisible = searchBoxVisible;
+        }
+    }
+}
